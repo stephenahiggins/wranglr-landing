f2ca7b7229653c51ecb288d728f679926f8205ea
Stephen Higgins <stephenahiggins@icloud.com>
2024-05-30 09:10:41 +0100

    Updated copy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
